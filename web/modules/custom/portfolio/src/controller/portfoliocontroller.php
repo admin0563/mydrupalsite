1003e77f44d403d2fad2fa126d5d3de389ea417a
aravind <aravind.sugu1999@gmail.com>
2025-06-17 11:03:23 -0400

    add PortfolioController to handle /portfolio route

--- web/modules/custom/portfolio/src/controller/portfoliocontroller.php
+++ web/modules/custom/portfolio/src/controller/portfoliocontroller.php
@@ -5,10 +5,9 @@ namespace Drupal\portfolio\Controller;
 use Drupal\Core\Controller\ControllerBase;
 
 class PortfolioController extends ControllerBase {
-  public function list() {
-    $build = [
-      '#markup' => '<h2>My Portfolio</h2><p>This is a custom portfolio page.</p>',
+  public function portfolioPage() {
+    return [
+      '#markup' => $this->t('Welcome to the Portfolio page!'),
     ];
-    return $build;
   }
 }
